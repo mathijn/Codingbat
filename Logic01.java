bc563054d8f02ff6a976742af68f452faa7213e8
Mathijn van Dijk <mathijnvandijk@outlook.com>
2017-03-21 20:24:59 +0100

    Logic01 opgaven 1 t/m 3

--- Logic01.java
+++ Logic01.java
@@ -38,5 +38,3 @@ squirrelPlay(95, true) → true
 public boolean squirrelPlay(int temp, boolean isSummer) {
   return (isSummer && (temp >= 60 && temp <=100)) || (!isSummer && (temp >=60 && temp <=90));
 }
-
-
